eb488d2d4078eab00ef8d791a41e25fac1c506ef
sajjad <faniam321@gmail.com>
2022-09-09 13:51:27 +0430

    add model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(UserAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    ordering = ('-date_joined',)
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(User, UserAdmin)
