6bb2ea6ec4b6e50b345f8686c7d70b59b45f9577
sajjad <faniam321@gmail.com>
2022-09-09 13:47:54 +0430

    fix bg

--- accounts/manager.py
+++ accounts/manager.py
@@ -8,7 +8,7 @@ class UserManager(BaseUserManager):
         if not username:
             raise ValueError('Usermust have an username')
         user = self.model(
-            email=self.normalize_email('email'),
+            email=self.normalize_email(email),
             username=username,
             first_name=first_name,
             last_name=last_name,
@@ -19,7 +19,7 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, first_name, last_name, username, email, password=None,):
         user = self.create_user(
-            email=self.normalize_email('email'),
+            email=self.normalize_email(email),
             first_name=first_name,
             last_name=last_name,
             username=username,
