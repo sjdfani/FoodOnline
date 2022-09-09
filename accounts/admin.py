6eb316283c5a958076032f2d33a79b3565e8b796
sajjad <faniam321@gmail.com>
2022-09-09 14:07:58 +0430

    fix bug

--- accounts/admin.py
+++ accounts/admin.py
@@ -6,9 +6,22 @@ from .models import User
 class UserAdmin(UserAdmin):
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
     ordering = ('-date_joined',)
+    readonly_fields = (
+        'date_joined', 'last_login', 'created_date', 'modified_date'
+    )
+    fieldsets = (
+        (('Personal Informations'), {
+            'fields': ('email', 'username', 'first_name', 'last_name', 'phone_number', 'role',)
+        }),
+        (('Date Informations'), {
+            'fields': ('date_joined', 'last_login', 'created_date', 'modified_date')
+        }),
+        (('Permissions Informations'), {
+            'fields': ('is_admin', 'is_staff', 'is_active', 'is_superadmin')
+        }),
+    )
+    list_filter = ['is_admin', 'is_staff', 'is_active', 'is_superadmin']
     filter_horizontal = ()
-    list_filter = ()
-    fieldsets = ()
 
 
 admin.site.register(User, UserAdmin)
